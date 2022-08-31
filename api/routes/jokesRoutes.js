0589dbf8811f81cd88f5b18c1744489c2da97af8
Angela Candela <angelacandela@hotmail.it>
2022-08-31 14:59:09 +0200

    Create 'jokes/random' route

--- api/routes/jokesRoutes.js
+++ api/routes/jokesRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jokesService = require('../services/jokesServices');
+// const connection = require('../../config/db');
 
 // Home
 router.get('/', (req, res) => {
@@ -11,6 +12,12 @@ router.get('/', (req, res) => {
 router.get('/all', async (req, res) => {
   const results = await jokesService.getAllJokes();
   res.send(results);
+});
+
+// Get random joke
+router.get('/random', async (req, res) => {
+  const result = await jokesService.getRandomJoke();
+  res.send(result);
 });
 
 module.exports = router;
