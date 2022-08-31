5d4f5c105f1652cac2d13bec57ad1575d4ab89e6
Angela Candela <angelacandela@hotmail.it>
2022-08-31 19:04:54 +0200

    Declare constants in script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,5 @@
-console.log('script.js loaded');
+const setup = document.getElementById("setup");
+const punchline = document.getElementById("punchline");
+const startButton = document.querySelector("button");
+const revealButton = document.getElementById("reveal-button");
+const nextButton = document.getElementById("next-button");
