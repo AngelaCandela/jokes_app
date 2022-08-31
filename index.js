97ba3cdc51f0630d405ae85dcb0d1441b7a76b0c
Angela Candela <angelacandela@hotmail.it>
2022-08-31 12:18:35 +0200

    Load the jokes router in index.js

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ const PORT = process.env.PORT;
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const jokes = require('./api/routes/jokesRoutes');
+app.use('/jokes', jokes);
