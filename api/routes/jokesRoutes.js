75080fc50e8bb95a80dfff8cc04aeee3776dc282
Angela Candela <angelacandela@hotmail.it>
2022-08-31 12:38:20 +0200

    Create 'jokes/all' route

--- api/routes/jokesRoutes.js
+++ api/routes/jokesRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const jokesService = require('../services/jokesServices');
 
 // Home
 router.get('/', (req, res) => {
   res.send('Welcome to my API!');
+});
+
+// Get all jokes
+router.get('/all', async (req, res) => {
+  const results = await jokesService.getAllJokes();
+  res.send(results);
 });
 
 module.exports = router;
