65b6a83742adc73e3a134f3ef4506ed22de21607
Angela Candela <angelacandela@hotmail.it>
2022-08-31 19:05:42 +0200

    Build client side js logic

--- public/script.js
+++ public/script.js
@@ -3,3 +3,63 @@ const punchline = document.getElementById("punchline");
 const startButton = document.querySelector("button");
 const revealButton = document.getElementById("reveal-button");
 const nextButton = document.getElementById("next-button");
+
+const showRevealButton = () => {
+  revealButton.classList.remove('hidden');
+};
+
+const showNextButton = () => {
+  nextButton.classList.remove('hidden');
+};
+
+const hideStartButton = () => {
+  startButton.classList.add('hidden');
+  showRevealButton();
+};
+
+const hideRevealButton = () => {
+  revealButton.classList.add('hidden');
+  showNextButton();
+};
+
+const hideNextButton = () => {
+  nextButton.classList.add('hidden');
+  showRevealButton();
+};
+
+const displaySetup = (data) => {
+  setup.innerText = data.setup;
+};
+
+const displayPunchline = (data) => {
+  punchline.innerText = data.punchline;
+  showNextButton();
+  hideRevealButton();
+};
+
+const getRandomJoke = () => {
+  fetch('/jokes/random', {
+    method: 'GET',
+    mode: 'cors',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+  })
+  })
+  .then(response => response.json())
+  .then(data => {
+    displaySetup(data);
+    hideStartButton();
+    revealButton.addEventListener("click", () => displayPunchline(data));
+  });
+};
+
+const restart = () => {
+  setup.innerText = "";
+  punchline.innerText = "";
+  getRandomJoke();
+  hideNextButton();
+};
+
+startButton.addEventListener("click", getRandomJoke);
+nextButton.addEventListener('click', restart);
