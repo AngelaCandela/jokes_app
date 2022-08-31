9bd0e873a44264909297fe9bfb588c1134f1e836
Angela Candela <angelacandela@hotmail.it>
2022-08-31 16:13:17 +0200

    Parse incoming request bodies with body-parser

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
