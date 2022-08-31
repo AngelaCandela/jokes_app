a5dacc7797440cd5dcbdc3e266f1888a59d7eed7
Angela Candela <angelacandela@hotmail.it>
2022-08-31 23:30:00 +0200

    Remove punchline p tag from index.html

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
 const setup = document.getElementById("setup");
-const punchline = document.getElementById("punchline");
 const startButton = document.querySelector("button");
 const revealButton = document.getElementById("reveal-button");
 const nextButton = document.getElementById("next-button");
